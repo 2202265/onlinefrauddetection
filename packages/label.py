e2ae035175235c976c98b37445b805220d2594a0
sarin-as <sarin72@gmail.com>
2022-06-09 12:37:19 +0100

    .

--- packages/label.py
+++ packages/label.py
@@ -1,7 +1,12 @@
 import boto3
 fraudDetector = boto3.client('frauddetector')
 
-def create_label():
+def create_label_fraud():
     fraudDetector.put_label(
-    name='string',
-    description='string')
+    name='fraud',
+    description='fraud')
+
+def create_label_legit():
+    fraudDetector.put_label(
+    name='legit',
+    description='legit')
