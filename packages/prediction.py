b8dea13247997b3ef206522387862cfaa7dacca2
sarin-as <sarin72@gmail.com>
2022-08-15 09:51:36 +0100

    Prediction API Update

--- packages/prediction.py
+++ packages/prediction.py
@@ -1,24 +1,26 @@
 import boto3
 fraudDetector = boto3.client('frauddetector')
-
-
 response = fraudDetector.get_event_prediction(
 detectorId='transaction_fraud_detector',
 detectorVersionId='1',
-eventId='32b7f7d1513fb4b2d5ed459ddc81fd06b9',
+eventId='32a4892528f4a8c57f7c991e91d2aa7b16',
 eventTypeName='transaction_event',
 entities=[
 {
 'entityType': 'customer',
-'entityId': '373-82-4202'
+'entityId': '174-88-0351'
 },
 ],
-eventTimestamp='2021-12-25T02:22:51Z',
+eventTimestamp='2021-12-11T14:46:25Z',
 eventVariables={
-'customer_email': 'navarropamela@powell.com',
-'order_price': '308.45',
-'payment_currency': 'INR',
-'billing_city':'Sneads Ferry'
+'customer_email': 'iflores@hotmail.com',
+'order_price': '105.44',
+'billing_city':'Ossian',
+'product_category':'grocery_pos',
+'billing_street':'4529 Brian Trail Suite 869',
+'user_agent':'Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.2; Trident/5.0)',
+'ip_address':'195.181.159.94',
+'card_bin':'300361'
 }
 )
 print(response)
